77dbe290583e0860850612d5c3d53ceb0fe7666a
texpertx <Asker.Khah@gmail.com>
2018-11-25 14:25:11 +0300

    Добавлены поля для фильтра

--- src/main/java/com/example/PartsServlet.java
+++ src/main/java/com/example/PartsServlet.java
@@ -14,6 +14,7 @@ import java.sql.*;
 @WebServlet("/")
 public class PartsServlet extends HttpServlet {
 
+    private static final String FIRST_DATE_FIELD_ID_SUFFIX = "_after";
     @Resource(name="jdbc/db")
     DataSource dataSource;
 
@@ -26,12 +27,50 @@ public class PartsServlet extends HttpServlet {
                     PreparedStatement preparedStatement = connection.prepareStatement("select * from parts");
                     ResultSet resultSet = preparedStatement.executeQuery()
             ) {
+                writeFilter(resultSet.getMetaData(), writer);
                 writeTable(resultSet, writer);
             } catch (SQLException e) {
                 throw new ServletException(e);
             }
             writer.write("</body>\n</html>");
         }
+    }
+
+    private void writeFilter(ResultSetMetaData metaData, PrintWriter writer) throws SQLException {
+        writer.format("<form action=\"%s\" method=\"GET\">\n", this.getClass().getSimpleName());
+        writer.write("<table>\n");
+        writer.write("<th>Filter</th>\n");
+        for (int i = 1; i <= metaData.getColumnCount(); i++) {
+            writer.write("<tr>\n");
+
+            writer.write("<td>");
+            String columnName = metaData.getColumnName(i);
+            String filterId = columnName;
+            if (metaData.getColumnType(i) == Types.DATE)
+                filterId += FIRST_DATE_FIELD_ID_SUFFIX;
+            writer.format("<label for=\"%s\">%s</label>", filterId, metaData.getColumnLabel(i));
+            writer.write("</td>");
+
+            writer.write("<td>");
+            switch (metaData.getColumnType(i)) {
+                case Types.INTEGER:
+                    writer.format("<input type=\"number\" name=\"%s\"/>", filterId);
+                    break;
+                case Types.VARCHAR:
+                    writer.format("<input type=\"text\" name=\"%s\"/>", filterId);
+                    break;
+                case Types.DATE:
+                    writer.format("<label for=\"%s\">after</label>\n<input type=\"date\" name=\"%s\"/>", filterId, filterId);
+                    writer.format("<label for=\"%s\">before</label>\n<input type=\"date\" name=\"%s\"/>", columnName + "_before", columnName + "_before");
+                    break;
+            }
+            writer.write("</td>");
+
+            writer.write("</tr>\n");
+        }
+        writer.write("</table>\n");
+        writer.write("<input type=\"submit\" value=\"Filter\"/>");
+        writer.write("<form>\n");
     }
 
     private void writeTable(ResultSet resultSet, PrintWriter writer) throws SQLException {
